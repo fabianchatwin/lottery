6002bcdc39c3ac6ae8b6aac6ea1bbc70de9be58d
fabian <fabianchatwin@gmail.com>
2022-12-13 17:44:31 +0100

    working

--- lottery.sol
+++ lottery.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Lottery{
-    address[] public players;
+    address[] players;
 
     function enter() public payable{
         require(msg.value > .01 ether);
@@ -17,5 +17,9 @@ contract Lottery{
         uint index = random() % players.length;
         payable (players[index]).transfer(address(this).balance);
         players = new address[](0);
+    }
+
+    function theplayers() public view returns (address[] memory) {
+        return players;
     }
 }
