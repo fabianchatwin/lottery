8189639dd4c9545b468eea646b48f5a577c8da4b
Fabian Chawin Cedrati <fabianchatwin@gmail.com>
2022-12-13 18:55:35 +0000

    first step 1

--- lottery.sol
+++ lottery.sol
@@ -2,24 +2,5 @@
 pragma solidity ^0.8.0;
 
 contract Lottery{
-    address[] players;
 
-    function enter() public payable{
-        require(msg.value > .01 ether);
-        players.push(msg.sender);
-    }
-
-    function random() private view returns(uint){
-        return uint (keccak256(abi.encode(block.timestamp,  players)));
-    }
-
-    function pickWinner() public {
-        uint index = random() % players.length;
-        payable (players[index]).transfer(address(this).balance);
-        players = new address[](0);
-    }
-
-    function theplayers() public view returns (address[] memory) {
-        return players;
-    }
 }
