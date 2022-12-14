eb303c6bbb519f42965ff43c341640b8889a5cf6
Fabian Chawin Cedrati <fabianchatwin@gmail.com>
2022-12-14 23:05:21 +0000

    step 1 interface

--- lottery.js
+++ lottery.js
@@ -1,6 +1,10 @@
-contractAddress = "";
+contractAddress = "0xe54A503767Eecbc96cbb23a478f07D6B4a1e6c26";
 
-window.addEventListener('load', async () => {
+/*
+*  funzione che viene eseguita dopo il caricamento della pagina
+*/ 
+window.addEventListener('load', async function() {
+	// controllo se esiste metamask
 	if (window.ethereum) {
 		window.web3 = new Web3(ethereum);
 		try {
@@ -9,6 +13,8 @@ window.addEventListener('load', async () => {
 		} catch (error) {
 			console.log("user rejected");
 		}
+	} else {
+		alert("No provider. Manca Metamask?");
 	}
 
   abi = JSON.parse( '[{"inputs":[],"name":"enter","outputs":[],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"pickWinner","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"theplayers","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"nonpayable","type":"function"}]' );
@@ -16,3 +22,6 @@ window.addEventListener('load', async () => {
   accounts = await web3.eth.getAccounts();
 });
 
+function deposit(){
+	alert("funziona");
+}
