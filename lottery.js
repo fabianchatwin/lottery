ec83468c174e7436b1fdaef7426b56a4d71a4b2c
Fabian Chawin Cedrati <fabianchatwin@gmail.com>
2022-12-15 00:24:17 +0000

    final

--- lottery.js
+++ lottery.js
@@ -27,3 +27,27 @@ window.addEventListener('load', async function() {
   // ottieni l'indirizzo di chi si connette 
   accounts = await web3.eth.getAccounts();
 });
+
+async function check() {
+	contractBalance = await web3.eth.getBalance(contractAddress);
+	document.getElementById("contractBalance").textContent = web3.utils.fromWei(contractBalance);
+
+	userBalance = await web3.eth.getBalance(accounts[0]);
+	document.getElementById("userBalance").textContent = web3.utils.fromWei(userBalance);
+
+	players = await contract.methods.theplayers().call();
+	document.getElementById("theplayers").textContent = players;
+}
+
+async function deposit() {
+	await contract.methods.enter().send({
+		from: accounts[0],
+		value: web3.utils.toWei("0.02")
+	})
+}
+
+async function play() {
+	await contract.methods.pickWinner().send( {
+		from: accounts[0]
+	})
+}
